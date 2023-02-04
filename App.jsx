ff219c754164784771d4b8cc6e8a28377ba104b7
IMRANDIL <aliimranadil2@gmail.com>
2023-02-04 13:31:02 +0530

    some more chagne

--- App.jsx
+++ App.jsx
@@ -6,10 +6,11 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './screens/Home';
 import ChatScreen from './screens/Chat';
 import Login from './screens/Login';
+import Signup from './screens/Signup';
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
+export default function App(props) {
   useEffect(() => {
     // console.log(config.API_KEY);
   }, []);
@@ -20,6 +21,12 @@ export default function App() {
         <Stack.Screen
           name="Login"
           component={Login}
+          options={{headerTitleAlign: 'center', headerShown: false}}
+          navigation={props.navigation}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={Signup}
           options={{headerTitleAlign: 'center', headerShown: false}}
         />
       </Stack.Navigator>
