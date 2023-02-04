feb50c5f434a1fc454d9f049604281dae4aa10cb
IMRANDIL <aliimranadil2@gmail.com>
2023-02-04 12:14:08 +0530

    added navigation

--- App.jsx
+++ App.jsx
@@ -1,14 +1,22 @@
 import {View, Text} from 'react-native';
 import React, {useEffect} from 'react';
 import config from './config/dotenvconfig';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './screens/Home';
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   useEffect(() => {
     // console.log(config.API_KEY);
   }, []);
 
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
