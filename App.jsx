1bd7b51ae586b63d5fb720cd4caa283c379c0e41
IMRANDIL <aliimranadil2@gmail.com>
2023-02-04 15:48:41 +0530

    some more change

--- App.jsx
+++ App.jsx
@@ -1,5 +1,4 @@
-import {View, Text} from 'react-native';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState, createContext, useContext} from 'react';
 import config from './config/dotenvconfig';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -7,9 +6,20 @@ import Home from './screens/Home';
 import ChatScreen from './screens/Chat';
 import Login from './screens/Login';
 import Signup from './screens/Signup';
+import {View, ActivityIndicator} from 'react-native';
+import {onAuthStateChanged} from 'firebase/auth';
 
 const Stack = createNativeStackNavigator();
 
+const AuthenticatedUserContext = createContext({});
+const AuthenticatedUserProvider = ({children}) => {
+  const [user, setUser] = useState(null);
+  return (
+    <AuthenticatedUserContext.Provider value={{user, setUser}}>
+      {children}
+    </AuthenticatedUserContext.Provider>
+  );
+};
 export default function App(props) {
   useEffect(() => {
     // console.log(config.API_KEY);
@@ -19,8 +29,8 @@ export default function App(props) {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name="Login"
-          component={Login}
+          name="Home"
+          component={Home}
           options={{headerTitleAlign: 'center', headerShown: false}}
           navigation={props.navigation}
         />
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -2,6 +2,9 @@ import React, {useEffect} from 'react';
 import {View, TouchableOpacity, Text, Image, StyleSheet} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import colors from '../colors';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import Entypo from 'react-native-vector-icons/Entypo';
+import IonIcon from 'react-native-vector-icons/Ionicons';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 const catImageUrl =
   'https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d';
@@ -13,7 +16,7 @@ const Home = () => {
     navigation.setOptions({
       headerLeft: () => (
         <FontAwesomeIcon
-          name="search"
+          icon={faSearch}
           size={24}
           color={colors.gray}
           style={{marginLeft: 15}}
@@ -37,7 +40,7 @@ const Home = () => {
       <TouchableOpacity
         onPress={() => navigation.navigate('Chat')}
         style={styles.chatButton}>
-        <Entypo name="chat" size={24} color={colors.lightGray} />
+        <IonIcon name="md-chatbubbles" size={24} color={colors.lightGray} />
       </TouchableOpacity>
     </View>
   );
