2bddc51bb9d5b12b9b8af84cbd3c5761a10d38aa
IMRANDIL <aliimranadil2@gmail.com>
2023-02-04 18:55:58 +0530

    some more change

--- screens/Chat.jsx
+++ screens/Chat.jsx
@@ -11,16 +11,35 @@ import {
 import {signOut} from 'firebase/auth';
 import {auth, database} from '../config/firebase';
 import {useNavigation} from '@react-navigation/native';
-import {AntDesign} from '@expo/vector-icons';
+import AntIcon from 'react-native-vector-icons/AntDesign';
 import colors from '../colors';
 
 const ChatScreen = () => {
   const [messages, setMessages] = useState([]);
   const navigation = useNavigation();
 
-  const signOut = () => {
+  const onSignOut = () => {
     signOut(auth).catch(error => console.log(error));
   };
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          style={{
+            marginRight: 10,
+          }}
+          onPress={onSignOut}>
+          <AntIcon
+            name="logout"
+            size={24}
+            color={colors.gray}
+            style={{marginRight: 10}}
+          />
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
 
   return <GiftedChat messages={messages} />;
 };
